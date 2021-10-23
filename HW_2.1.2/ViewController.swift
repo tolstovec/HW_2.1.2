bf23bb04c5101c0d93aeadb0a539cd205021d711
Ilya Pokhodin <ilya.pokhodin@gmail.com>
2021-10-23 21:33:18 +0300

    Риализована логика смены цветов и название кнопки, но проблема с размером шрифта и констрейнтами

--- HW_2.1.2/ViewController.swift
+++ HW_2.1.2/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redLightSignal: UIView!
+    @IBOutlet var yellowLightSignal: UIView!
+    @IBOutlet var greenLightSignal: UIView!
+    @IBOutlet var changeButtonText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightSignal.alpha = 0.3
+        yellowLightSignal.alpha = 0.3
+        greenLightSignal.alpha = 0.3
+        
+        redLightSignal.layer.cornerRadius = 70
+        yellowLightSignal.layer.cornerRadius = 70
+        greenLightSignal.layer.cornerRadius = 70
+        
+        changeButtonText.setTitle("START", for: .normal)
     }
 
-
+    @IBAction func changeLight() {
+        if redLightSignal.alpha == 0.3
+            && yellowLightSignal.alpha == 0.3
+            && greenLightSignal.alpha == 0.3 {
+            
+            redLightSignal.alpha = 1
+        }
+        
+        changeButtonText.setTitle("NEXT", for: .normal)
+        
+        if redLightSignal.alpha == 1 {
+            redLightSignal.alpha = 0.3
+            yellowLightSignal.alpha = 1
+        } else if yellowLightSignal.alpha == 1 {
+            yellowLightSignal.alpha = 0.3
+            greenLightSignal.alpha = 1
+        } else {
+            greenLightSignal.alpha = 0.3
+            redLightSignal.alpha = 1
+        }
+        
+    }
+    
+    
 }
 
